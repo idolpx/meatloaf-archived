5885e48266c5009729c7b7421fd5a6651fe72ff3
James Johnston <jjohnston@techknowpro.com>
2021-05-25 23:40:11 -0500

    Fix crash when CBM is off

--- lib/IECBus/iec.cpp
+++ lib/IECBus/iec.cpp
@@ -1,3 +1,23 @@
+// Meatloaf - A Commodore 64/128 multi-device emulator
+// https://github.com/idolpx/meatloaf
+// Copyright(C) 2020 James Johnston
+//
+// This file is part of Meatloaf but adapted for use in the FujiNet project
+// https://github.com/FujiNetWIFI/fujinet-platformio
+// 
+// Meatloaf is free software : you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Meatloaf is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Meatloaf. If not, see <http://www.gnu.org/licenses/>.
+
 #include "iec.h"
 
 using namespace CBM;
@@ -475,8 +495,21 @@ IEC::ATNCheck  IEC::checkATN(ATNCmd& atn_cmd)
 	delayMicroseconds(1);
 #endif
 
-	if(status(IEC_PIN_ATN) == pulled) {
+	// Checks if CBM is sending a reset (setting the RESET line high). This is typically
+	// when the CBM is reset itself. In this case, we are supposed to reset all states to initial.
+	if(status(IEC_PIN_RESET) == pulled) 
+	{
+		if (status(IEC_PIN_ATN) == pulled)
+		{
+			// If RESET & ATN are both pulled then CBM is off
+			return ATN_IDLE;
+		}
+		
+		return ATN_RESET;
+	}
 
+	if (status(IEC_PIN_ATN) == pulled)
+	{
 		// Attention line is pulled, go to listener mode and get message.
 		// Being fast with the next two lines here is CRITICAL!
 		pull(IEC_PIN_DATA);
