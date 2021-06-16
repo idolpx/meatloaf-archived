d28da67f00b20e39474dd0c75e4a2d3b940a5da0
pszczygielski1 <qus123@gmail.com>
2021-06-16 18:02:01 +0200

    url FS implementation

--- lib/filesystem/media/url.h
+++ lib/filesystem/media/url.h
@@ -3,6 +3,8 @@
 
 #include "meat_io.h"
 #include "EdUrlParser.h"
+#include "stream_writer.h"
+#include "../../include/make_unique.h"
 
 
 /********************************************************
@@ -27,6 +29,48 @@ public:
 
 
 private:
+};
+
+
+/********************************************************
+ * File implementations
+ ********************************************************/
+
+class UrlFile : public MFile {
+public:
+    UrlFile(std::string path) : MFile(path) {};
+    MIstream* createIStream(MIstream* src) override;
+    bool isDirectory() override { return true; };
+    bool rewindDirectory() { return true; } ;
+    MFile* getNextFileInDir() override ;
+    bool mkDir() override { return false; };
+    bool exists() override { return true; };
+
+    // all these below are achievable with some trickery
+    time_t getLastWrite() override ;
+    time_t getCreationTime() override ;
+    size_t size() override { return 0; };
+    bool remove() override { return false; };
+    bool rename(const char* dest) { return false; };
+
+    MFile* cd(std::string newDir) override {
+        // cding into urlfile will always just point you to the link read from contents of the file...
+        return pointedFile.get();
+    }
+
+private:
+    std::unique_ptr<MFile> pointedFile;
+
+    MFile* getPointed() {
+        if(pointedFile == nullptr) {
+            std::unique_ptr<MIstream> istream(inputStream());
+            auto reader = std::make_unique<StreamReader>(istream.get());
+            auto linkUrl = reader->readLn();
+            pointedFile.reset(MFSOwner::File(linkUrl));
+        }
+        return pointedFile.get();
+    }
+
 };
 
 #endif
