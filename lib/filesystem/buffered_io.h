ba7ee0a9946b0f2842f4b3fad18a70162f387930
idolpx <jaime@idolpx.com>
2021-06-17 16:16:35 -0500

    Add seek to MBuffer Add readByte to BufferedReader

--- lib/filesystem/buffered_io.h
+++ lib/filesystem/buffered_io.h
@@ -25,6 +25,25 @@ public:
 
     MBuffer() {};
 
+    bool seek(uint32_t pos, SeekMode mode) {
+        switch (mode) {
+            case SeekSet:
+                buffer = &buffer[0] + pos;
+                break;
+
+            case SeekCur:
+                buffer = *&buffer + pos;
+                break;
+            
+            case SeekEnd:
+                buffer = &buffer[len] - pos;
+                break;
+        }
+    };
+    bool seek(uint32_t pos) {
+        return seek(pos, SeekSet);
+    }
+
     char& operator [](int idx) {
         return buffer[idx];
     }
@@ -105,6 +124,15 @@ public:
             refillBuffer();
 
         return &mbuffer;
+    }
+    uint8_t readByte() {
+        if(!eofOccured)
+            refillBuffer();
+
+        uint32_t i = 1;
+        uint8_t b = mbuffer[0];
+        mbuffer.seek(i, SeekCur);
+        return b;
     }
     bool eof() {
         return eofOccured;
